81186f4420b65787f297f8e5d8afbce2c39effd1
zipian <alfian.rpl23@yaoo.com>
2024-10-18 03:09:34 +0700

    Update

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,36 @@
+import Link from "next/link"
+
+export default function Navigations(){
+    return(
+        <nav className="flex justify-between p-4 m-2">
+            <div className="brand-name">
+                <span className="font-semibold text-xl">
+                    MakeIt
+                    <span className="bg-teal-300">Easy</span>
+                </span>
+            </div>
+            <div className="nav-home">
+                <ul className="flex gap-5">
+                    <li>
+                        <Link
+                        href="/">
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                        href="/tugas">
+                            Service
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                        href="/deadline">
+                            About us
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
